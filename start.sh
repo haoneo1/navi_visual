ae0ff359955eb6063c91b13c575b348051ca9a20
Liuxy <ansilxy@163.com>
2025-12-09 23:19:15 +0800

    alpha 0.4

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# waiting for 192.168.0.39
+while ! ping -c 1 -W 1 192.168.0.39 >/dev/null 2>&1; do
+    echo 'waiting for service'
+    sleep 1
+done
+
+# start core-cap & core-gos
+# check and kill 
+ssh user@192.168.0.39 "pgrep -f '\$HOME/core-cap' | xargs -r kill -9"
+ssh user@192.168.0.39 "pgrep -f '\$HOME/core-gos' | xargs -r kill -9"
+
+# strat core-cap
+ssh user@192.168.0.39 "nohup ~/core-cap > ~/core-cap.log 2>&1 &"
+
+sleep 1
+
+# start core-gos
+ssh user@192.168.0.39 "nohup ~/core-gos > ~/core-gos.log 2>&1 &"
+
+sleep 1
+
+# start navi
+uv run main.py
